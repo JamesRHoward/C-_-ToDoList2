4ae3a58a9504fb822c813d90b8a68254479552d4
James Howard and Jesse Mckay <james+jesse@>
2016-11-16 11:46:08 -0800

    started day two of todolist

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,19 +8,22 @@ namespace ToDoList
   {
     public HomeModule()
     {
-      Get["/"] = _ => View["add_new_task.cshtml"];
-      Get["/view_all_tasks"] = _ => {
-        List<string> allTasks = Task.GetAll();
-        return View["view_all_tasks.cshtml", allTasks];
+      Get["/"] = _ => {
+        return View["index.cshtml"];
       };
+      Get["/tasks"] = _ => {
+        List<Task> allTasks = Task.GetAll();
+        return View["task_form.cshtml", allTasks];
+      };
+      Get["/tasks/new"] = _ => {
+        return View ["task_form.cshtml"];
+      };
+      };
+
       Post["/task_added"] = _ => {
         Task newTask = new Task (Request.Form["new-task"]);
         newTask.Save();
         return View["task_added.cshtml", newTask];
-      };
-      Post["/tasks_cleared"] = _ => {
-        Task.ClearAll();
-        return View["tasks_cleared.cshtml"];
       };
     }
   }
